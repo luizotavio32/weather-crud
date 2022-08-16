c843e6aed03502d5ef02b6c3ec28bafcf31e0e0a
Luiz Otavio <luizotavioindiana@hotmail.com>
2022-08-16 00:54:04 -0300

    feat: add return frm requisiton

--- src/routes.js
+++ src/routes.js
@@ -8,9 +8,30 @@ router.get('/weather/:city', async (req, res) => {
     // #swagger.tags = ['City']
     const city = req.params.city;
 
-    let objectReturn = await weatherController.getWeatherByCity(city);
 
-    res.send(objectReturn)
+    let objectReturn =  await weatherController.getWeatherByCity(city)
+
+
+    /* #swagger.responses[404] = {
+        description: 'City not found'
+    } */
+    if (objectReturn.code === '404') {
+    
+        return res.status(404).send(objectReturn)
+    }
+
+    /* #swagger.responses[200] = {
+        schema: { $ref: "#/definitions/CityWeather" },
+        description: 'City Weather returned sucesfully'
+    } */
+
+    return res.status(200).send(objectReturn)
+
+
+
+
+
+
 
 })
 
