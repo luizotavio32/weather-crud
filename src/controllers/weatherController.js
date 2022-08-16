e35e923a21cedf5bcc1f3bde348d2a56ff09c55b
Luiz Otavio <luizotavioindiana@hotmail.com>
2022-08-16 00:55:03 -0300

    feat: add axios requisition to weatherController

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -1,18 +1,28 @@
+const axios = require('axios');
+require('custom-env').env()
+
 class WeatherController {
 
     static async getWeatherByCity(city) {
 
-        
-        if (typeof city === 'string' || city instanceof String) {
-            return "its a string"
-        }
-        else {
-            return "not a string"
-        }
-        
+        let cityWeatherObject;
 
+        try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}`)
+            cityWeatherObject = {
+                "cityName": response.data.name,
+                "weather": response.data.main
+            }
+        } catch (error) {
+            console.log(error)
+            cityWeatherObject = {
+                "code": error.response.data.cod,
+                "message": error.response.data.message
+            }
+          }
+        
+        return cityWeatherObject
     };
 }
 
-
-module.exports = WeatherController;
+module.exports = WeatherController
